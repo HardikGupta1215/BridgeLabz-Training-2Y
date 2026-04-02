0eff2452f14bd1b063df69258cd0a172afb95abc
HardikGupta1215 <hardhitterhardik12@gmail.com>
2026-04-02 15:07:54 +0530

    Add files via upload

---
+++ IntroDSA/Average.java
@@ -0,0 +1,11 @@
+public class Average {
+    public static double avg(int arr[]) {
+        int sum = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        return (double) sum / arr.length;
+    }
+}
